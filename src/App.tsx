b43744079c0e63e2ab444cc5456d2880629356a9
Alex Irkhin <irhin_a@garpix.com>
2022-01-20 14:28:43 +0300

    Fixed notes

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,31 @@
 import React from 'react';
 import {Button, Dropdown, Input} from './components/UI';
-import { CloseIcon, FullIcon, LogoutIcon, NoFullIcon, SortIcon } from './images';
+import {INPUT_NAME} from './const';
+import {CloseIcon, FullIcon, LogoutIcon, NoFullIcon, SortIcon} from './images';
 
 const App = () => {
   return (
     <>
-      <Input type="password" name="test" label="Test" />
-      <Input optional={true} type="password" name="test" label="Test" />
+      <Input type="password" name={INPUT_NAME.PASSWORD} label="Test" />
+      <Input optional={true} type="password" name={INPUT_NAME.PASSWORD} label="Test" />
       <Button>Войти</Button>
       <Button loading={true}>Войти</Button>
-      <Button theme='transparent'><FullIcon /></Button>
-      <Button theme='transparent'><NoFullIcon /></Button>
-      <Button theme='transparent'><CloseIcon /></Button>
-      <Button theme='transparent' iconLeft={<SortIcon />}>Форматировать</Button>
-      <Button theme='transparent' iconRight={<LogoutIcon />}>Выйти</Button>
-      <Dropdown title='Test' status={true}/>
+      <Button theme="transparent">
+        <FullIcon />
+      </Button>
+      <Button theme="transparent">
+        <NoFullIcon />
+      </Button>
+      <Button theme="transparent">
+        <CloseIcon />
+      </Button>
+      <Button theme="transparent" iconLeft={<SortIcon />}>
+        Форматировать
+      </Button>
+      <Button theme="transparent" iconRight={<LogoutIcon />}>
+        Выйти
+      </Button>
+      <Dropdown title="Test" status={true} />
     </>
   );
 };
