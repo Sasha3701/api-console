547a94c7294830bf453ffe782780aeecf464b81b
Alex Irkhin <irhin_a@garpix.com>
2022-01-21 10:37:35 +0300

    Added redux-persist

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import GlobalStyle from './styles';
-import store from './store/store';
+import store, {persistor} from './store/store';
 import {Provider} from 'react-redux';
 import {BrowserRouter} from 'react-router-dom';
+import {PersistGate} from 'redux-persist/integration/react';
 
 const app = (
   <BrowserRouter>
     <Provider store={store}>
-      <GlobalStyle />
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <GlobalStyle />
+        <App />
+      </PersistGate>
     </Provider>
   </BrowserRouter>
 );
--- src/store/store.ts
+++ src/store/store.ts
@@ -2,11 +2,22 @@ import {createStore, applyMiddleware} from 'redux';
 import createSagaMiddleware from '@redux-saga/core';
 import rootReducer from './reducers/rootReducer';
 import {rootSaga} from './sagas/rootSaga';
+import {persistStore, persistReducer} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const sagaMiddleware = createSagaMiddleware();
 
-const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+const store = createStore(persistedReducer, applyMiddleware(sagaMiddleware));
 
 sagaMiddleware.run(rootSaga);
+
+export const persistor = persistStore(store);
 
 export default store;
